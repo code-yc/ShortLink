b864b73a041d51c2ecfd760480063bea9c434cd4
YC <code.yc@outlook.com>
2025-02-20 05:15:08 +0800

    feat: 开发用户自定义异常码

---
+++ admin/src/main/java/com/yc/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -0,0 +1,28 @@
+package com.yc.shortlink.admin.common.enums;
+
+import com.yc.shortlink.admin.common.convention.errorcode.IErrorCode;
+
+public enum UserErrorCodeEnum implements IErrorCode {
+
+    USER_NULL("B000200", "用户记录不存在");
+
+
+    private final String code;
+
+    private final String message;
+
+    UserErrorCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
