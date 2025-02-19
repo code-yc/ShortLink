1b6f75ab2d66d373868bbcf0df46ea0f4ad26323
YC <code.yc@outlook.com>
2025-02-20 05:29:29 +0800

    feat: 查询用户信息脱敏功能

--- admin/src/main/java/com/yc/shortlink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/com/yc/shortlink/admin/dto/resp/UserRespDTO.java
@@ -1,5 +1,7 @@
 package com.yc.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.yc.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 import java.util.Date;
@@ -27,6 +29,7 @@ public class UserRespDTO {
     /**
      * 手机号
      */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
