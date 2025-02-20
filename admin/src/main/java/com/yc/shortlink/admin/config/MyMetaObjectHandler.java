7693f3e4ec4b2f524d109a3d7bf437d496754070
YC <code.yc@outlook.com>
2025-02-21 03:46:36 +0800

    feat: 持久层实体对象 属性自动填充

---
+++ admin/src/main/java/com/yc/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,24 @@
+package com.yc.shortlink.admin.config;
+
+import cn.hutool.core.date.DateTime;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
