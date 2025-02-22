3bdeba59f759ae674849171d34d1e0bb8f50afb1
YC <code.yc@outlook.com>
2025-02-23 03:39:38 +0800

    feat: 查询分组功能

--- admin/src/main/java/com/yc/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/yc/shortlink/admin/controller/GroupController.java
@@ -3,11 +3,15 @@ package com.yc.shortlink.admin.controller;
 import com.yc.shortlink.admin.common.convention.result.Result;
 import com.yc.shortlink.admin.common.convention.result.Results;
 import com.yc.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import com.yc.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.yc.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 短链接分组控制层
@@ -18,10 +22,20 @@ public class GroupController {
 
     private  final GroupService groupService;
 
+    /**
+     * 新增短链接分组
+     */
     @PostMapping("/api/short-link/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
         groupService.saveGroup(requestParam.getName());
         return Results.success();
     }
 
+    /**
+     * 查询用户短链接分组集合
+     */
+    @GetMapping("/api/short-link/v1/group")
+    public Result<List<ShortLinkGroupRespDTO>> listGroup() {
+        return Results.success(groupService.listGroup());
+    }
 }
--- admin/src/main/java/com/yc/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/yc/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,14 +1,20 @@
 package com.yc.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.BeanUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yc.shortlink.admin.dao.entity.GroupDO;
 import com.yc.shortlink.admin.dao.mapper.GroupMapper;
+import com.yc.shortlink.admin.database.BaseDO;
+import com.yc.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.yc.shortlink.admin.service.GroupService;
 import com.yc.shortlink.admin.util.RandomGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 短链接分组接口实现层
@@ -25,9 +31,22 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         } while (!hasGid(gid));
         GroupDO groupDO = GroupDO.builder()
                 .gid(gid)
+                .sortOrder(0)
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
+    }
+
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {// TODO 从当前上下文获取用户名
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                .eq(GroupDO::getUsername, null)
+                .orderByDesc(List.of(GroupDO::getSortOrder, BaseDO::getUpdateTime));
+
+        List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+
+        return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
     }
 
     private boolean hasGid(String gid) {
