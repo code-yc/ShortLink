15bb6013c11eacf4d8e50d7aa1f4d276fa767416
YC <code.yc@outlook.com>
2025-02-23 03:16:38 +0800

    feat: 分组新增功能

--- admin/src/main/java/com/yc/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/yc/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,9 +1,12 @@
 package com.yc.shortlink.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yc.shortlink.admin.dao.entity.GroupDO;
 import com.yc.shortlink.admin.dao.mapper.GroupMapper;
 import com.yc.shortlink.admin.service.GroupService;
+import com.yc.shortlink.admin.util.RandomGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +16,26 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+
+    @Override
+    public void saveGroup(String groupName) {
+        String gid;
+        do {
+            gid = RandomGenerator.generateRandom();
+        } while (!hasGid(gid));
+        GroupDO groupDO = GroupDO.builder()
+                .gid(gid)
+                .name(groupName)
+                .build();
+        baseMapper.insert(groupDO);
+    }
+
+    private boolean hasGid(String gid) {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getGid, gid)
+                // TODO 设置用户名
+                .eq(GroupDO::getUsername, null);
+        GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
+        return hasGroupFlag == null;
+    }
 }
